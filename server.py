f43adfabbf92c324f77c89dd5fe64b576bbaba0d
Marcos Sombra <markos.vs@live.com>
2019-05-22 16:17:43 -0300

    [Beta] Exibição de resultados (apenas visual)

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if(request.method == 'POST'):
+        r = True
+    else:
+        r = False
+    return render_template('index.html', active=1, results=r)
 
 
 if(__name__ == '__main__'):
