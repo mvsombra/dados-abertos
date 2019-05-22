18580a24fb5e1324f2cc43cccb3bbcb2a0635d93
Marcos Sombra <markos.vs@live.com>
2019-05-22 17:14:04 -0300

    [Beta] Names de Formulários + Listagem completa de Municípios

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,6 +11,11 @@ def index():
     else:
         r = False
     return render_template('index.html', active=1, results=r)
+
+
+@app.before_request
+def before_request():
+    g.municipios = pd.read_csv('static/municipios.csv')['municipio']
 
 
 if(__name__ == '__main__'):
