a9e805c0fd038c59be5ab15e02aa391100f1dacf
Marcos Sombra <markos.vs@live.com>
2019-05-23 09:22:43 -0300

    [Teste] Tentativa de identificação de Bug - parte 3

--- server.py
+++ server.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if(request.method == 'POST'):
-        return render_template('index.html', active=1, results=True)
+        return render_template('index.html', active=1, results=1)
     else:
-        return render_template('index.html', active=1, results=False)
+        return render_template('index.html', active=1, results=0)
 
 
 @app.before_request
