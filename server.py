0a2ab56a6af1b90dff96f290aaf9fbbccb681c63
Marcos Sombra <markos.vs@live.com>
2019-05-23 09:46:22 -0300

    [Beta] Gerenciamento de requisições entre formulários

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, url_for, redirect
 import pandas as pd
 
 app = Flask(__name__)
@@ -7,7 +7,12 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if(request.method == 'POST'):
-        return render_template('index.html', active=1, results=1)
+        if(request.form['tipo'] == 'dados'):
+            return render_template('index.html', active=1, results=1)
+        elif(request.form['tipo'] == 'avancado'):
+            return render_template('index.html', active=2, results=1)
+        else:
+            return redirect(url_for('index'))
     else:
         return render_template('index.html', active=1, results=0)
 
