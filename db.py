0b95989f36d8845b873e3a3cfa4732678c1af440
Marcos Sombra <marcos.sombraaa@gmail.com>
2019-10-26 17:14:13 -0300

    [Beta] Configurando SQLAlchemy

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
 
 Base = declarative_base()
@@ -7,9 +8,23 @@ Base = declarative_base()
 
 class Licitacao(Base):
     __tablename__ = 'licitacoes'
-    id = Column('id', Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    edital = Column(String)
+    municipio = Column(Integer, ForeignKey('municipios.id'))
+    objeto = Column(String)
+    modalidade = Column(String)
+    orgao = Column(String)
+    data_abertura = Column(Date)
+    status = Column(String)
 
 
 class DB:
     def __init__(self):
         self.engine = create_engine(os.environ['DB_URL'])
+        self.Session = sessionmaker(bind=self.engine)
+
+    def get_licitacoes(self):
+        session = self.Session()
+        lics = session.query(Licitacao).all()
+        session.close()
+        return lics
--- licitacao.py
+++ licitacao.py
@@ -1,22 +1,11 @@
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Integer, ForeignKey, String, Column, Date
 import datetime
 
 Base = declarative_base()
 
 
 class Licitacao(Base):
-    __tablename__ = 'licitacoes'
-    id = Column(Integer, primary_key=True)
-    edital = Column(String)
-    municipio = Column(Integer, ForeignKey('municipios.id'))
-    objeto = Column(String)
-    modalidade = Column(String)
-    entidade = Column(String)
-    data_abertura = Column(Date)
-    status = Column(String)
-
     def __init__(self, i, mun, ed, obj, mod, org, dt, sts):
         self.id = i
         self.edital = ed
